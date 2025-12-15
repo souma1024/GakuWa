590765bb4ea0d56def5dd68025c460e6e69e463d
katsukiLOVE <katsu1392@icloud.com>
2025-12-15 13:40:45 +0900

    ほんとうろく

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import healthRoutes from './healthRoutes'
 import dbRoutes from './dbRoutes'
+import { userController } from '../controllers/userController'
 
 const router = Router()
 
 // /api/health
 router.use('/health', healthRoutes)
 router.use('/db', dbRoutes)
+// /
+router.post('/api/register',userController);
 
 export default router
