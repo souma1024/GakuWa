86df4e5ab412f2a7fed31e4520dedc2984de1f83
shiryu-tanaka <shiryu.1211@icloud.com>
2025-12-18 11:47:20 +0900

    index.tsの修正

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import { login }from '../controllers/authController'
 import otpRoutes from './otpRoutes';
 import authRoutes from './authRoutes';
+import { verifyOtp } from '../controllers/otpController';
+import { getMe } from '../controllers/authMeController';
 
 const router = Router();
 
@@ -13,5 +15,11 @@ router.use('/auth/otp', otpRoutes);
 
 // /api/auth
 router.use('/auth', authRoutes);
+
+// /api/auth/otp/verify
+router.post('/verify', verifyOtp);
+
+// /api/auth/me
+router.get('/me', getMe);
 
 export default router;
