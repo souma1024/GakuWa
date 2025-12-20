1062832a0a740f84f43c3d97a9c2bec824aedfd5
souma <souma3789522@gmail.com>
2025-12-20 16:10:40 +0900

    create session middleware

--- apps/backend/src/middlewares/authMiddleware.ts
+++ apps/backend/src/middlewares/authMiddleware.ts
@@ -7,16 +7,16 @@ import { sendError } from "../utils/response";
 // ログイン済みかどうかを検証する
 // ログイン済みじゃないとアクセスできないAPI（eventやprofileなど）の前に呼び出す。
 export const authenticateUser = async (req: Request, res: Response, next: NextFunction) => {
-  const { sessionToken } = req.cookies.sessionToken;
+  const sessionToken = req.cookies?.session_id;
   
   if (!sessionToken) {
-    sendError(res, 'authentication_error', 'セッション情報が保存されていません。', 403);
+    return sendError(res, 'authentication_error', 'セッション情報が保存されていません。', 403);
   }
 
   const result = await sessionService.checkSession(sessionToken);
   if (!result.success) {
-    sendError(res, 'authentication_error', result.message,  403);
+    return sendError(res, 'authentication_error', result.message,  403);
   }
 
-  next();
+  return next();
 };
--- apps/backend/src/services/sessionService.ts
+++ apps/backend/src/services/sessionService.ts
@@ -8,22 +8,22 @@ type checkSessionResult =
 const SESSION_TTL_MS = 1000 * 60 * 60 * 24 * 7; // 7日
 
 function generateSessionToken(): string {
-  return crypto.randomBytes(32).toString("hex");
+  return crypto.randomBytes(32).toString("hex");  
 }
 
 export const sessionService = {
-  async createSession(user: any) {
+  async createSession(id: bigint) {
 
     const sessionToken = generateSessionToken();
     const expiresAt = new Date(Date.now() + SESSION_TTL_MS);
 
     const session = await sessionRepository.createSession({
-      userId: user.id,
+      userId: id,
       sessionToken,
       expiresAt,
     });
 
-    return { sessionToken, expiresAt };
+    return session.sessionToken;
   },
 
   async checkSession(sessionToken: string): Promise<checkSessionResult> {
