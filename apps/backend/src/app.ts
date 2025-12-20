d8612ef1f661da89ca9a57cc0b089d690cec8dd1
souma <souma3789522@gmail.com>
2025-12-20 16:15:07 +0900

    modify

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -6,12 +6,12 @@ import cookieParser from 'cookie-parser';
 const app = express()
 
 // JSON を受け取るための設定
-app.use(express.json())
+app.use(express.json());
 
 //クッキーを使えるようにする
 app.use(cookieParser())
 
 // /api から下は全部 routes に委譲
-app.use('/api', routes)
+app.use('/api', routes);
 
 export default app
