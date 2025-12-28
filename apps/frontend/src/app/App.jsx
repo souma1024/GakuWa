b5ec1c6fa8bdb9cc9d679622501f8e68eeeb26f9
Sunao-Maeda <ep24118-2440@gs4e.chubu.ac.jp>
2025-12-28 19:39:50 +0900

    記事削除API

--- apps/frontend/src/app/App.jsx
+++ apps/frontend/src/app/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+
 import SignupPage from "../pages/SignupPage";
 import OtpVerifyPage from "../pages/OtpVerifyPage";
 import UserHomePage from "../pages/UserHomePage";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import BlockPage from "../components/BlockPage";
+import ArticlesList from "../pages/ArticlesList";
 
 function App() {
   return (
@@ -14,18 +16,22 @@ function App() {
 
       {/* ログイン */}
       <Route path="/login" element={<LoginPage />} />
-      
+
       {/* 新規登録 */}
       <Route path="/signup" element={<SignupPage />} />
-      
+
       {/* OTP入力画面 */}
       <Route path="/signup/otp-verify" element={<OtpVerifyPage />} />
-      
-      {/* ユーザーホーム画面 */}
+
+      {/* ログイン必須エリア */}
       <Route element={<BlockPage />}>
+        {/* ユーザーホーム */}
         <Route path="/:handle" element={<UserHomePage />} />
+
+        {/* ★ 記事一覧 */}
+        <Route path="/articles" element={<ArticlesList />} />
       </Route>
-      
+
       {/* 404 Not Found */}
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
