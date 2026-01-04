c1bf7733b227a4cea8f1b01af2ff1d8518e6c720
Sunao-Maeda <ep24118-2440@gs4e.chubu.ac.jp>
2026-01-04 17:50:55 +0900

    コンフリクト解消2

---
+++ apps/backend/src/controllers/adminTagController.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { adminTagService } from "../services/adminTagService";
+import { sendSuccess } from "../utils/sendSuccess";
+
+export const updateTagController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tagId = Number(req.params.tagId);
+    const { name } = req.body;
+
+    const updated = await adminTagService.updateTag(tagId, name);
+    sendSuccess(res, updated);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteTagController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tagId = Number(req.params.tagId);
+    await adminTagService.deleteTag(tagId);
+    sendSuccess(res, { message: "タグを削除しました" });
+  } catch (err) {
+    next(err);
+  }
+};
