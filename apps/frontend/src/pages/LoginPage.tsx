31e6ed40478b376b31db85396515cf4b68b37c8f
souma <souma3789522@gmail.com>
2026-01-08 02:47:37 +0900

    modify response data format

--- apps/frontend/src/pages/LoginPage.tsx
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -51,9 +51,8 @@ export default function LoginPage() {
 
       const result = await response.json();
 
-    console.log("login result: FULL", result);
+      const user = result.data.userInfo;
 
-      const user = result.data;
       console.log("user情報 %o", user)
       // public_tokenを取得してOTP画面に遷移
       if (result.success && user.handle) {
