25d192ceebf174a07ed04e344b84a2f17a3b9763
souma <souma3789522@gmail.com>
2026-01-08 05:12:29 +0900

    modify page navigation

--- apps/frontend/src/pages/BlockPage.tsx
+++ apps/frontend/src/pages/BlockPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { User } from "../components/ProfileCard";
 
 export type OutletContext = {
@@ -9,12 +9,11 @@ export type OutletContext = {
 
 export default function BlockPage() {
   const [user, setUser] = useState<User | null>(null);
-  
+  const navigate = useNavigate();
 
   useEffect(() => {
     const check = async () => {
       try {
-
         const response = await fetch("http://localhost:8080/api/auth/session", {
           method: "POST",
           credentials: "include",
@@ -22,10 +21,20 @@ export default function BlockPage() {
         const result = await response.json();
 
         if (result.success) {
-          history.replaceState(result.data, "", "/" + result.data.handle)
-          setUser(result.data)
+          setUser(result.data);
+
+          const handle = result.data.handle;
+          const path = window.location.pathname;
+
+          const m = path.match(/^\/([^\/]+)(\/.*)?$/);
+          const pathHandle = m?.[1];
+
+          const isUserArea = pathHandle === handle; // 自分のhandle配下だけ許可
+
+          if (isUserArea) return;
+
+          navigate(`/${handle}`, { state: user});
         } 
-        console.log("BlockPageに到達したよ");
       } catch(e) {
         console.log(e);
       }
