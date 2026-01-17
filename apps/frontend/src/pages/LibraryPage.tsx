55c2332fad48b779a803172eba894042e776c0e1
souma <souma3789522@gmail.com>
2026-01-18 05:05:17 +0900

    create library pages

--- apps/frontend/src/pages/LibraryPage.tsx
+++ apps/frontend/src/pages/LibraryPage.tsx
@@ -1,12 +1,29 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useArticles } from "../hooks/useArticles";
+import  ArticleCard  from "../components/ArticleCard";
 import styles from "../styles/library.module.css";
-import ArticlePage from "./ArticlePage";
+
 
 export default function LibraryPage() {
+  const location = useLocation();
+  const user = location.state;
+  const { articles, fetchUsersArticles } = useArticles();
+
+  useEffect(() => {
+    fetchUsersArticles(user.handle);
+  }, [])
 
   return (
    <>
-    <div>
-      
+    <div className={ styles.main }>
+      <div className={ styles.articles }>
+        {
+          articles.map((article, _) => (
+            <ArticleCard key={ article.handle } article={ article }/>
+          ))
+        }
+      </div>
     </div>
    </> 
   );
