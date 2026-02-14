0e865c6a8ddd989024b3ee6333ed18d5fc4df03c
souma1024 <souma3789522@gmail.com>
2026-02-14 16:00:55 +0900

    create article publish function

--- apps/frontend/src/components/ArticleStatus.tsx
+++ apps/frontend/src/components/ArticleStatus.tsx
@@ -1,20 +1,39 @@
+import { useState } from "react";
 import styles from "../styles/piece.module.css";
 
 type Props = {
-  status: string
+  status: string,
+  handle: string
 }
 
-export default function ArticleStatus(status: Props) {
-  const statusText = status.status;
+export default function ArticleStatus({ handle, status}: Props) {
+  const [statusText, setStatusText] = useState(status);
 
   // 記事を公開する関数
-  function publish() {
+  async function publish() {
+    try {
+      const res = await fetch(`http://localhost:8080/api/articles/publish`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ handle: handle }),
+      });
 
+      if (!res) {
+        console.log("失敗");
+      }
+
+      const result = await res.json();
+      setStatusText(result.data.status);
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   return (
     <>
-      <div className={`${ styles.status } ${styles[statusText] ?? ""}`} >
+      <div className={`${ styles.status } ${styles[statusText] ?? ""}`} onClick={publish}>
         <p className={ styles.text }>{ statusText }</p>
       </div>
     </>
