2284a0f605cab06ca4f53b26c08f2c4edda53672
souma <souma3789522@gmail.com>
2026-07-02 16:22:15 +0900

    記事ランキングを修正しました。

--- apps/backend/src/repositories/rankingRepository.ts
+++ apps/backend/src/repositories/rankingRepository.ts
@@ -1,29 +1,33 @@
 import { prisma } from '../lib/prisma';
 
+type UserArticleRanking = {
+  id: bigint;
+  handle: string;
+  avatarUrl: string | null;
+  articles_count: bigint;
+};
+
 export const rankingRepositoy = {
   async postsRanking() {
-    const ranking = await prisma.user.findMany({
-      select: {
-        id: true,
-        handle: true,
-        avatarUrl: true,
-        _count: {
-          select: {
-            articles: {
-              where: {
-                status: 'published',
-              },
-            },
-          },
-        },
-      },
-      orderBy: {
-        articles: {
-          _count: 'desc',
-        },
-      },
-      take: 10,
-    });
+    const ranking = await prisma.$queryRaw<UserArticleRanking[]>`
+      SELECT
+        u.id,
+        u.handle,
+        u.avatar_url AS avatarUrl,
+        COALESCE(a._count, 0) AS articles_count
+      FROM users u
+      LEFT JOIN (
+        SELECT
+          author_id,
+          COUNT(*) AS _count
+        FROM articles
+        WHERE status = 'published'
+        GROUP BY author_id
+      ) a
+      ON u.id = a.author_id
+      ORDER BY articles_count DESC
+      LIMIT 10
+    `;
     return ranking;
   }
 }
